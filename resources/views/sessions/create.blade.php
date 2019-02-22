1bd3512ac434c41cea439d4a1263d58f38b91557
vagrant <vagrant@homestead>
2019-02-22 02:56:16 +0000

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -9,9 +9,7 @@
             </div>
             <div class="card-body">
                 @include('shared._errors')
-
                 <form method="POST" action="{{ route('login') }}">
-
                     {{ csrf_field() }}
 
                     <div class="form-group">
@@ -24,8 +22,14 @@
                         <input type="password" name="password" class="form-control" value="{{ old('password') }}">
                     </div>
 
-                    <button type="submit" class="btn btn-primary">登录</button>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">记住我</label>
+                        </div>
+                    </div>
 
+                    <button type="submit" class="btn btn-primary">登录</button>
                 </form>
 
                 <hr>
