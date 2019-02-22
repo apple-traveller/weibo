2e5ae5b197da6633b9d728413478bd7d48ae384d
vagrant <vagrant@homestead>
2019-02-22 02:14:18 +0000

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    //
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+        //dd($credentials);
+        /**
+         * array:2 [▼
+                   "email" => "18571632539@163.com"
+                   "password" => "123456"
+               ]
+         */
+        if (Auth::attempt($credentials)) {
+            // 登录成功后的相关操作
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            // 登录失败后的相关操作
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+
+    }
+}
