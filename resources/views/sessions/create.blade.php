e590fa78e36ba03f552ae313c1a77c3c8bc70fdd
vagrant <vagrant@homestead>
2019-02-23 05:26:42 +0000

    重置密码

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -9,6 +9,7 @@
             </div>
             <div class="card-body">
                 @include('shared._errors')
+
                 <form method="POST" action="{{ route('login') }}">
                     {{ csrf_field() }}
 
@@ -18,7 +19,7 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="password">密码：</label>
+                        <label for="password">密码（<a href="{{ route('password.request') }}">忘记密码</a>）：</label>
                         <input type="password" name="password" class="form-control" value="{{ old('password') }}">
                     </div>
 
